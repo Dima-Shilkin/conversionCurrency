f26dd4c0993240dd0ef134c494b9dc4b13ba560c
Dima Shilkin <DimaSchilkin@yandex.ru>
2024-09-11 15:47:34 +0300

    Готовый проект на чистом JS

---
+++ js/state.js
@@ -0,0 +1,19 @@
+export default {
+  url: 'https://v6.exchangerate-api.com/v6/877b7ed8a7e1306f4ebfe24e',
+  codes: [],
+  pair: {
+    from: '',
+    to: '',
+  },
+  amount: '',
+  loading: false,
+  currentTab: 'convert',
+  currency: {
+    code: 'USD',
+  },
+  currencies: ['USD', 'EUR', 'RUB'],
+  actions: {
+    remove: 'remove',
+    change: 'change',
+  },
+};
